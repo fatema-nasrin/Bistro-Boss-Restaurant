a8e83a9d180a37cfa24f8b1ab1b0de9efbbd0d5a
fatema-nasrin <nasrinfatema35@gmail.com>
2023-10-11 16:17:26 +0600

    featured components added

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -5,7 +5,7 @@ const Featured = () => {
     return (
         <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle subheading="Check it out" heading="Featured Item"/>
-            <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-25 py-20 px-36">
+            <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-60 py-20 px-36">
                 <div>
                     <img src={featuredImage} alt="" />
                 </div>
@@ -13,7 +13,7 @@ const Featured = () => {
                     <p>Aug 20, 2023</p>
                     <p className="uppercase">what can i get </p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad exercitationem officia enim quaerat vero culpa ipsum atque deleniti libero fugiat?Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ullam natus similique alias accusamus, eius minus tenetur quas saepe assumenda! </p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 text-white">Order Now</button>
                 </div>
             </div>
         </div>
