dc32d4a1dfc699c368fe52cbd52c7cb5f16ad107
fatema-nasrin <nasrinfatema35@gmail.com>
2023-10-20 20:22:12 +0600

    custom hook added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     const navOptions = <>
      <li>
+                <Link to="/">Home</Link>
+              </li>
+     <li>
                 <Link to="/menu">Our Menu</Link>
               </li>
               <li>
